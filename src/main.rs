a1995f87054ea6c5582b12a52e2b3dfb9930785d
Satoshi Warita <div9851.tech@gmail.com>
2023-01-06 19:17:52 +0900

    respond to multiple PINGs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use std::{io::Write, net::TcpListener};
+use std::{io::Read, io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
+            Ok(mut _stream) => loop {
+                let mut buf = [0; 256];
+                _stream.read(&mut buf).unwrap();
                 _stream.write(b"+PONG\r\n").unwrap();
-            }
+            },
             Err(e) => {
                 println!("error: {}", e);
             }
