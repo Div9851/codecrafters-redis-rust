c84f071bae22cc14672c3f8ce38ea79e334053bd
Satoshi Warita <div9851.tech@gmail.com>
2023-01-07 21:07:51 +0900

    handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use std::{io::Read, io::Write, net::TcpListener};
+use std::{io::Read, io::Write, net::TcpListener, net::TcpStream, thread};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => loop {
-                let mut buf = [0; 256];
-                _stream.read(&mut buf).unwrap();
-                _stream.write(b"+PONG\r\n").unwrap();
-            },
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        let stream = stream.unwrap();
+        thread::spawn(move || handle_connection(stream));
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 256];
+    loop {
+        stream.read(&mut buf).unwrap();
+        stream.write(b"+PONG\r\n").unwrap();
     }
 }
